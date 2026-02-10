e3627e93111a6ba6710e2baac034939b53be6ab8
laurawada <laura.wada@germinare.org.br>
2026-02-09 23:48:24 -0300

    fix: correct file name and :hover styling

---
+++ src/components/Buttons/ButtonRedirect.tsx
@@ -0,0 +1,14 @@
+import "./Button-redirect.css";
+import { Link } from "react-router-dom";
+
+interface ButtonRedirectProps {
+    title: string;
+    redirect: string;
+}
+
+export default function ButtonRedirect({ title, redirect } : ButtonRedirectProps) {
+    
+    return (
+        <Link to={redirect} className="button-redirect">{title}</Link>
+    )
+}
