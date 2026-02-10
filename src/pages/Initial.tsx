693143a34e5b15b32fa954bef67395d404ac274c
laurawada <laura.wada@germinare.org.br>
2026-02-09 23:50:51 -0300

    feat: add information card and switching from span to div

--- src/pages/Initial.tsx
+++ src/pages/Initial.tsx
@@ -1,49 +1,24 @@
 import '../styles/Initial.css'
-import ButtonRedirect from '../components/Buttons/Button-redirect';
-
+import ButtonRedirect from '../components/Buttons/ButtonRedirect';
+import InformationCard from '../components/information/InformationCard';
 export default function Initial() {
     return (
         <div className="initial-page">
             <div className="initial-card"> 
                 <img src="/llbj-logo.svg" alt="Logo Colégio" id="logo-escola"/>
                 <h1>Um futuro melhor para seus filhos!</h1>
-                <p>Nossa escola é um espaço onde o conhecimento, os valores e o <br></br>respeito caminham juntos na construção do futuro.</p>
+                <p id="texto-escola">Nossa escola é um espaço onde o conhecimento, os valores e o respeito caminham juntos na construção do futuro.</p>
                 <div className="container">
                     <img src="/criancas.png" alt="Crianças com livro" id="criancas"/>
-                    <span className="initial-login">
+                    <div className="initial-login">
                         <img src="/coruja.svg" alt="Coruja LLBJ" id="coruja-foto"/>
                         <ButtonRedirect title='Comece aqui!' redirect='/login'></ButtonRedirect>
                         <p>Encontre experiências reais e vá além dos livros!</p>
-                    </span>
-                    {/* <img src="/adolescente.png" alt="Adolescente com livro" id="adolescente"/>  */}
+                    </div>
+                    <img src="/adolescente.png" alt="Adolescente com livro" id="adolescente"/> 
                 </div>
             </div>
-            <div className="information-card">
-                <div className="students">
-                    <span>
-                        <img src="/escola-icone.svg" alt="Ícone escola" />
-                        +7,5 mil
-                    </span>
-                    <br />
-                    alunos prontos para novas experiências
-                </div>
-                <div className="school-position">
-                    <span>
-                        <img src="/livro-icone.svg" alt="Ícone livro" />
-                        #10
-                    </span>
-                    <br />
-                    melhor escola de São Paulo
-                </div>
-                <div className="medals">
-                    <span>
-                        <img src="/trofeu-icone.svg" alt="Ícone trofeu" />
-                        +50
-                    </span>
-                    <br />
-                    medalhas em olimpíadas científicas
-                </div>
-            </div>
+            <InformationCard></InformationCard>
         </div>
     );
 }
