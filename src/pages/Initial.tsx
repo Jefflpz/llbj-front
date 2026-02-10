970a56dd945d6c4ad1e3d9a97bf1641544ad550e
laurawada <laura.wada@germinare.org.br>
2026-02-10 10:27:03 -0300

    fix: switching from id to classname, adding const text

--- src/pages/Initial.tsx
+++ src/pages/Initial.tsx
@@ -1,21 +1,28 @@
 import '../styles/Initial.css'
 import ButtonRedirect from '../components/Buttons/ButtonRedirect';
 import InformationCard from '../components/information/InformationCard';
+
 export default function Initial() {
+
+    const text = {
+        title: "Um futuro melhor para seus filhos!",
+        description: "Nossa escola é um espaço onde o conhecimento, os valores e o respeito caminham juntos na construção do futuro."
+    }
+
     return (
         <div className="initial-page">
             <div className="initial-card"> 
-                <img src="/llbj-logo.svg" alt="Logo Colégio" id="logo-escola"/>
-                <h1>Um futuro melhor para seus filhos!</h1>
-                <p id="texto-escola">Nossa escola é um espaço onde o conhecimento, os valores e o respeito caminham juntos na construção do futuro.</p>
+                <img src="/llbj-logo.svg" alt="Logo Colégio" className="logo-escola"/>
+                <h1>{text.title}</h1>
+                <p className="texto-escola">{text.description}</p>
                 <div className="container">
-                    <img src="/criancas.png" alt="Crianças com livro" id="criancas"/>
+                    <img src="/criancas.png" alt="Crianças com livro" className="criancas"/>
                     <div className="initial-login">
-                        <img src="/coruja.svg" alt="Coruja LLBJ" id="coruja-foto"/>
+                        <img src="/coruja.svg" alt="Coruja LLBJ" className="coruja-foto"/>
                         <ButtonRedirect title='Comece aqui!' redirect='/login'></ButtonRedirect>
                         <p>Encontre experiências reais e vá além dos livros!</p>
                     </div>
-                    <img src="/adolescente.png" alt="Adolescente com livro" id="adolescente"/> 
+                    <img src="/adolescente.png" alt="Adolescente com livro" className="adolescente"/> 
                 </div>
             </div>
             <InformationCard></InformationCard>
