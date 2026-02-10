c39f8f3f80728491fd75c5d240202e6f8d1da426
laurawada <laura.wada@germinare.org.br>
2026-02-09 22:07:39 -0300

    feat: add landing page

--- src/pages/Initial.tsx
+++ src/pages/Initial.tsx
@@ -1,11 +1,48 @@
+import '../styles/Initial.css'
+import ButtonRedirect from '../components/Buttons/Button-redirect';
+
 export default function Initial() {
     return (
         <div className="initial-page">
-            <div className="initial-card">
-                <img src="/llbj-logo.svg" alt="Logo Colégio" />
-                <h1>Bem-vindo ao sistema de gerenciamento de biblioteca do Colégio LLBJ!</h1>
-                <p>Faça login para acessar as funcionalidades do sistema ou clique no link abaixo para criar uma nova conta.</p>
-                <a href="/login">Fazer Login</a>
+            <div className="initial-card"> 
+                <img src="/llbj-logo.svg" alt="Logo Colégio" id="logo-escola"/>
+                <h1>Um futuro melhor para seus filhos!</h1>
+                <p>Nossa escola é um espaço onde o conhecimento, os valores e o <br></br>respeito caminham juntos na construção do futuro.</p>
+                <div className="container">
+                    <img src="/criancas.png" alt="Crianças com livro" id="criancas"/>
+                    <span className="initial-login">
+                        <img src="/coruja.svg" alt="Coruja LLBJ" id="coruja-foto"/>
+                        <ButtonRedirect title='Comece aqui!' redirect='/login'></ButtonRedirect>
+                        <p>Encontre experiências reais e vá além dos livros!</p>
+                    </span>
+                    {/* <img src="/adolescente.png" alt="Adolescente com livro" id="adolescente"/>  */}
+                </div>
+            </div>
+            <div className="information-card">
+                <div className="students">
+                    <span>
+                        <img src="/escola-icone.svg" alt="Ícone escola" />
+                        +7,5 mil
+                    </span>
+                    <br />
+                    alunos prontos para novas experiências
+                </div>
+                <div className="school-position">
+                    <span>
+                        <img src="/livro-icone.svg" alt="Ícone livro" />
+                        #10
+                    </span>
+                    <br />
+                    melhor escola de São Paulo
+                </div>
+                <div className="medals">
+                    <span>
+                        <img src="/trofeu-icone.svg" alt="Ícone trofeu" />
+                        +50
+                    </span>
+                    <br />
+                    medalhas em olimpíadas científicas
+                </div>
             </div>
         </div>
     );
