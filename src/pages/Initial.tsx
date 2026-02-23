f1a2e084cef6eddec78487d3163e2bea0639a553
laurawada <laura.wada@germinare.org.br>
2026-02-22 22:27:24 -0300

    fix: adicionando terceira página e espaço na segunda pagina

--- src/pages/Initial.tsx
+++ src/pages/Initial.tsx
@@ -1,11 +1,13 @@
 import FirstPage from "./Initial/FirstPage";
 import SecondPage from "./Initial/SecondPage";
+import ThirdPage from "./Initial/ThirdPage";
 
 export default function Initial() {
     return (
         <div className="Initial">
             <FirstPage></FirstPage>
             <SecondPage></SecondPage>
+            <ThirdPage></ThirdPage>
         </div>
     );
 }
